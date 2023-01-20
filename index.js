bcf9cce4c6a98c58800575005a00cb2556a68349
ikhushisingla <singlakhushi822@gmail.com>
2023-01-20 11:02:14 +0530

    final commit

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const loadText=document.querySelector('.loading-text')
-const bg=document.querySelector('.bg')
+const boxes = document.querySelectorAll('.box')
 
-let load=0
-let int =setInterval(blurring,30)
+window.addEventListener('scroll' , checkBoxes)
 
-function blurring(){
-    load++
 
-    if(load>99){
-        clearInterval(int)
-    }
+function checkBoxes(){
+    const triggerBottom = window.innerHeight / 5 * 4
 
-    loadText.innerText=`${load}%`
-    loadText.style.opacity = scale( load,0,100,1,0)
-    bg.style.filter =`blur(${scale(load,0,100,30,0)}px)`
-}
+    boxes.forEach(box=>{
+        const boxTop=box.getBoundingClientRect().top
 
-const scale =(num,in_min,in_max,out_min,out_max) =>{
-    return(num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        if(boxTop<triggerBottom){
+            box.classList.add('show')
+        } else{
+            box.classList.remove('show')
+        }
+    })
 }
